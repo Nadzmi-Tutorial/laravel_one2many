c6a07ef691f41f5fce5d4e3223a8801e460004d6
Nadzmi Idzham <nadzmiidzham@gmail.com>
2019-09-28 21:23:14 +0800

    finish

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,39 @@
 |
 */
 
+use App\User;
+use App\Post;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/insert', function() {});
+Route::get('/insert', function() {
+    $user = User::findOrFail(1);
+    $post = new Post(['title' => 'My first post', 'body' => 'This is my first post']);
+
+    $user->posts()->save($post);
+});
+
+Route::get('/update', function() {
+    $user = User::findOrFail(1);
+
+    // $user->posts()->whereId(1)->update(['title' => 'I love laravel', 'body' => 'This is awesome']);
+    $user->posts()->where('id', '=', 2)->update(['title' => 'I love laravel 2', 'body' => 'This is awesome 2']);
+});
     
-Route::get('/update', function() {});
+Route::get('/read', function() {
+    $user = User::findOrFail(1);
+
+    // return $user->posts;
+
+    foreach($user->posts as $post) {
+        echo $post->title . '<br />';
+    }
+});
     
-Route::get('/read', function() {});
-    
-Route::get('/delete', function() {});
+Route::get('/delete', function() {
+    $user = User::findOrFail(1);
+
+    $user->posts()->whereId(1)->delete();
+});
